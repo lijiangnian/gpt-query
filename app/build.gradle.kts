e28fc23d7a554cb3ca4811b3aee6cd121ba47ba3
lijiangnian <lijiangnian001@gmail.com>
2026-08-24 23:17:16 +0800

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.vsme.vlinkconverter"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vsme.vlinkconverter"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
