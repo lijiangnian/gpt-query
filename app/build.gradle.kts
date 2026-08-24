1006fd07b83d321e593093665d7292ca8ed7b056
lijiangnian <lijiangnian001@gmail.com>
2026-08-25 00:10:16 +0800

    Bump rewritten VLink to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.vsme.vlinkconverter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "2.0.0"
     }
 }
 
